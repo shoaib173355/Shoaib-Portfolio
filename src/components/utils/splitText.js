ef19fb46de76d8a740c1fb8e98643884fd62e458
Shoaib <shoaib173355@gmail.com>
2026-03-13 16:55:12 +0530

    fix gsap trail issue

--- src/components/utils/splitText.js
+++ src/components/utils/splitText.js
@@ -1,7 +1,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
-import { SplitText } from "gsap-trial/SplitText";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
